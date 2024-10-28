cc626c5adf023e066559ab45929ad4dcf00cbfa5
Даниил <defender.0599@gmail.com>
2024-10-28 12:38:49 +0300

    class_House

--- module_5.3.py
+++ module_5.3.py
@@ -48,27 +48,26 @@ class House:
             return self
         return NotImplemented
 
-# Пример выполнения кода
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
 print(h1)
 print(h2)
 
-print(h1 == h2)  # eq
+print(h1 == h2)  # __eq__
 
-h1 = h1 + 10  # add
+h1 = h1 + 10  # __add__
 print(h1)
 print(h1 == h2)
 
-h1 += 10  # iadd
+h1 += 10  # __iadd__
 print(h1)
 
-h2 = 10 + h2  # radd
+h2 = 10 + h2  # __radd__
 print(h2)
 
-print(h1 > h2)  # gt
-print(h1 >= h2)  # ge
-print(h1 < h2)  # lt
-print(h1 <= h2)  # le
-print(h1 != h2)  # ne
+print(h1 > h2)  # __gt__
+print(h1 >= h2)  # __ge__
+print(h1 < h2)  # __lt__
+print(h1 <= h2)  # __le__
+print(h1 != h2)  # __ne__
