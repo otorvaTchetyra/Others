c38aa07f5f78d0154342470b6039f6d098400abb
Даниил <defender.0599@gmail.com>
2024-11-01 12:42:10 +0300

    change 38 line

--- module_5_3_.py
+++ module_5_3_.py
@@ -3,8 +3,8 @@ class House:
         self.name = name
         self.floors = floors
 
-    def __repr__(self):
-        return f'Название: {self.name}, кол-во этажей: {self.floors}'
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.floors}"
 
     def __eq__(self, other):
         if isinstance(other, House):
@@ -12,62 +12,64 @@ class House:
         return False
 
     def __lt__(self, other):
-        if isinstance(other, (House, int)):
-            return self.floors < (other.floors if isinstance(other, House) else other)
-        return NotImplemented
+        if isinstance(other, House):
+            return self.floors < other.floors
+        return False
 
     def __le__(self, other):
-        if isinstance(other, (House, int)):
-            return self.floors <= (other.floors if isinstance(other, House) else other)
-        return NotImplemented
+        if isinstance(other, House):
+            return self.floors <= other.floors
+        return False
 
     def __gt__(self, other):
-        if isinstance(other, (House, int)):
-            return self.floors > (other.floors if isinstance(other, House) else other)
-        return NotImplemented
+        if isinstance(other, House):
+            return self.floors > other.floors
+        return False
 
     def __ge__(self, other):
-        if isinstance(other, (House, int)):
-            return self.floors >= (other.floors if isinstance(other, House) else other)
-        return NotImplemented
+        if isinstance(other, House):
+            return self.floors >= other.floors
+        return False
 
     def __ne__(self, other):
-        return not self.__eq__(other)
+        if isinstance(other, House):
+            return self.floors != other.floors
+        return True
 
-    def __add__(self, value):
-        if isinstance(value, int):
-            return House(self.name, self.floors + value)
-        return NotImplemented
-
-    def __radd__(self, value):
-        return self.__add__(value)
-
-    def __iadd__(self, value):
+    def add(self, value):
         if isinstance(value, int):
             self.floors += value
-            return self
-        return NotImplemented
+        return self
 
+    def __radd__(self, value):
+        return self.add(value)
+
+    def __iadd__(self, value):
+        return self.add(value)
+
+# Пример выполняемого кода:
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
 print(h1)
 print(h2)
 
-print(h1 == h2)  # __eq__
+print(h1 == h2)  # eq
 
-h1 = h1 + 10  # __add__
+h1 = h1 + 10  # add
 print(h1)
 print(h1 == h2)
 
-h1 += 10  # __iadd__
+h1 += 10  # iadd
 print(h1)
 
-h2 = 10 + h2  # __radd__
+h2 = 10 + h2  # radd
 print(h2)
 
-print(h1 > h2)  # __gt__
-print(h1 >= h2)  # __ge__
-print(h1 < h2)  # __lt__
-print(h1 <= h2)  # __le__
+print(h1 > h2)  # gt
+print(h1 >= h2)  # ge
+print(h1 < h2)  # lt
+print(h1 <= h2)  # le
+print(h1 != h2)  # ne
+
 print(h1 != h2)  # __ne__
